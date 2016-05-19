63b6870db9f13a7b7ec997a003f5de5813dc3cea
Murray Wynnes <mrwynnes@gmail.com>
2016-05-19 16:22:24 +0100

    attempt to initialize gulp buid scripts on node buildpack

--- public/js/all.js
+++ public/js/all.js
@@ -1,17 +1,1 @@
-$(function(){
-    setTimeout(function () {
-            $('.intro-header')
-                .css({'visibility':'visible'})
-                .addClass('animated fadeInDown');
-        }, 1000
-    );
-    setTimeout(function () {
-            $('.img-responsive')
-                .css({'visibility':'visible'})
-                .addClass('animated bounceIn');
-        }, 500
-    );
-    $('[data-toggle="tooltip"]').tooltip();
-});
-
-//# sourceMappingURL=all.js.map
+$(function(){setTimeout(function(){$(".intro-header").css({visibility:"visible"}).addClass("animated fadeInDown")},1e3),setTimeout(function(){$(".img-responsive").css({visibility:"visible"}).addClass("animated bounceIn")},500),$('[data-toggle="tooltip"]').tooltip()});
